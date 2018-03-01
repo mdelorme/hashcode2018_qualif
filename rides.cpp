946953f58e1d59a69c08ebb7e9a6ea12215a6f18
Maxime Delorme <m.delorme@surrey.ac.uk>
2018-03-01 17:59:12 +0000

    Classe point

--- rides.cpp
+++ rides.cpp
@@ -2,17 +2,27 @@
 
 int R, C, F, B, T, N;
 
+struct Point {
+  int row, col;
+};
+
 struct Ride {
-  int row_start, col_start, row_end, col_end, start_t, finish_t;
+  Point start;
+  Point end;
+  int start_t, finish_t;
 
   void print() {
-    std::cerr << "(" << row_start << "; " << col_start << "); ("
-	      << "(" << row_end   << "; " << col_end << "); "
+    std::cerr << "(" << start.row << "; " << start.col << "); ("
+	      << "(" << end.row   << "; " << end.col << "); "
 	      << start_t << " " << finish_t << std::endl;
   }
 };
 
 std::vector<Ride> rides;
+
+int time_to_finish_ride(Ride &r, Point &car_pos) {
+
+}
 
 int main(int argc, char **argv) {
   std::ifstream input_file;
@@ -27,7 +37,7 @@ int main(int argc, char **argv) {
 
   for (int i=0; i < N; ++i) {
     Ride r;
-    std::cin >> r.row_start >> r.col_start >> r.row_end >> r.col_end
+    std::cin >> r.start.row >> r.start.col >> r.end.row >> r.end.col
 	     >> r.start_t >> r.finish_t;
     rides.push_back(r);
     r.print();
@@ -35,6 +45,8 @@ int main(int argc, char **argv) {
 
   if (argc > 1)
     input_file.close();
+
+  
 
   return 0;
 }
